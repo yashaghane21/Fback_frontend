ef27a28256cd5a0811c9d616423ea893f0d50512
Yash aghane <yashaghane21@gmail.com>
2023-10-15 18:39:49 +0530

    h

--- src/Components/Dash.jsx
+++ src/Components/Dash.jsx
@@ -4,9 +4,9 @@ import img from "../assets/qq.png"
 const Dash = () => {
     const { theme } = useAuth()
     return (
-        <div className={` ${theme == "light" ? "bg-[#f5f1f0]" : " bg-[#0c131d]"} flex sm:h-[100vh] h-[65vh]  flex-col-reverse sm:flex-row  p-5`}>
-            <div className='w-[100%] sm:w-[50%] p-3 flex justify-center items-center'>
-                <img src={img} className='w-[80%] sm:w-[90%] h-[100%] border-2 border-black rounded-2xl sm:h-[50%]' />
+        <div className={` ${theme == "light" ? "bg-[#f5f1f0]" : " bg-[#0c131d]"} flex sm:h-[100vh] h-[85vh]  flex-col-reverse sm:flex-row  p-5`}>
+            <div className='w-[100%] sm:w-[50%] h-[50vh] sm:h-[100vh] p-3 flex justify-center items-center'>
+                <img src={img} className='w-[80%] sm:w-[90%] h-[50%] border-2 border-black rounded-2xl sm:h-[50%]' />
             </div>
             <div className='w-[100%] sm:w-[55%] p-2 sm:p-5 flex flex-col justify-center items-center'>
                 <h1 className='font-bold text-xl  text-blue-600 sm:text-4xl'>Analyzed Feedbacks</h1>
